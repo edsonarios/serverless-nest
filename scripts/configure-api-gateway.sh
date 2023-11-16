f55015df6763e06c651ef7603c5c4ed76b1fc7fd
edsonarios <edsonrios9@gmail.com>
2023-11-16 18:17:45 -0400

    Fix path 4

--- scripts/configure-api-gateway.sh
+++ scripts/configure-api-gateway.sh
@@ -4,7 +4,8 @@ API_ID=$1
 STAGE_NAME=$2
 BURST_LIMIT=$3
 RATE_LIMIT=$4
-
+echo "Configuring rate limiting for aws:"
+echo $API_ID $STAGE_NAME $BURST_LIMIT $RATE_LIMIT
 aws apigateway update-stage \
   --rest-api-id $API_ID \
   --stage-name $STAGE_NAME \
