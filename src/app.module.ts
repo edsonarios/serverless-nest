427f91b3c43d8d29f17112a1a84155af8eec0f15
edsonarios <edsonrios9@gmail.com>
2023-11-16 15:30:17 -0400

    Added rate limit for all endpoints in nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { RelacionalModule } from './medy/relacional/relacional.module'
+import rateLimit from 'express-rate-limit'
 
 @Module({
   imports: [RelacionalModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        rateLimit({
+          windowMs: 15 * 60 * 1000, // 15 minutos
+          max: 6, // Límite de 100 solicitudes por ventana de tiempo
+        }),
+      )
+      .forRoutes('*') // Aplica a todas las rutas
+  }
+}
